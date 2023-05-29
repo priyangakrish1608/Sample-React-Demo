68173bcbcb5c8ca3060e71166658d2f11d1f01e7
priyangakrish1608 <103566466+priyangakrish1608@users.noreply.github.com>
2023-05-29 16:18:58 +0530

    employee changes

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -1,73 +1,71 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
-import './editPage.css'
+import './editPage.css';
+
 
 const EditPage = () => {
-  const location = useLocation();
-  const [rowData, setRowData] = useState(null);
-  const [Id, SetID] = useState('');
+const location = useLocation();
+const [rowData, setRowData] = useState(null);
+const [Id, SetID] = useState('');
+const getRowDataFromQueryParams = useCallback((search, id) => {
+const data = [
+ { id: 1, name: 'Loy', sex: "Female", email: 'loy@example.com', salary: '25000', department: 'Sales'},
+ { id: 2, name: 'Aswin', sex: "Male", email: 'aswin@example.com', salary: '25000', department: 'Accounts' },
+ { id: 3, name: 'Preethi', sex: "Female", email: 'preethi@example.com', salary: '27000', department: 'Testing' },
+ { id: 4, name: 'Chandharan', sex: "Male", email: 'chandharan@example.com', salary: '30000', department: 'HR' },
+ {id: 5, name: 'Sreesha', sex: "Female", email: 'sreesha@example.com', salary: '10000', department: 'Supporting'},
+ {id: 6, name: 'Amutha', sex: "Female", email: 'aumtha@example.com', salary: '15000', department: 'Supporting'},
+ {id: 7, name: 'shakthi', sex: "Male", email: 'shakthi@example.com', salary: '32000', department: 'Project Manager'}
+ ];
+ // Retrieve the corresponding rowData based on the id from your data source (e.g., API, Redux store)
+ const rowData = data.find((e) => e.id === id) ?? {};
+ return rowData;
+}, []);
 
-  const getRowDataFromQueryParams = useCallback((search) => {
-    const data = [
-      { id: 1, name: 'John Doe', sex: "M", email: 'john@example.com', salary: '15000', department: 'Sales' },
-      { id: 2, name: 'Jane Smith', sex: "M", email: 'jane@example.com', salary: '25000', department: 'Accounts'  },
-      { id: 3, name: 'Bob Johnson', sex: "M", email: 'bob@example.com', salary: '27000', department: 'Testing'},
-      { id: 4, name: 'Alice Williams', sex: "M", email: 'alice@example.com', salary: '30000', department: 'HR'  },
-    ];
-    const queryParams = new URLSearchParams(search);
-    const id = queryParams.get('id');
-    SetID(id);
-    console.log(id);
-    console.log(Id);
-
-    // Retrieve the corresponding rowData based on the id from your data source (e.g., API, Redux store)
-    const rowData = data.find((e) => e.id === id) ?? {};
-    return rowData;
-  }, [Id]);
-
-  useEffect(() => {
-    const rowDataFromLocation = location.state?.rowData;
-    const rowDataFromQueryParams = getRowDataFromQueryParams(location.search);
-    console.log(new URLSearchParams(location.search).get('id'));
-    setRowData(rowDataFromLocation || rowDataFromQueryParams);
-  }, [location, getRowDataFromQueryParams]);
-
-  if (!rowData) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h2>{new URLSearchParams(location.search).get('id') === '' ? 'Add' : 'Edit'} Page</h2>
-      <form>
+useEffect(() => {
+const queryParams = new URLSearchParams(location.search);
+const id = queryParams.get('id');
+SetID(id);
+const rowDataFromLocation = location.state?.rowData;
+const rowDataFromQueryParams = getRowDataFromQueryParams(location.search, id);
+ setRowData(rowDataFromLocation || rowDataFromQueryParams);
+}, [location, getRowDataFromQueryParams]);
+if (!rowData) {
+return <div>Loading...</div>;
+ }
+return (
+   <div>
+     <div className='container' style={{ width: '700px' }}>
+     <h2>{Id === '' ? 'Add' : 'Edit'} Page</h2>
+     <form>
         <div>
-          <label>ID:</label>
-          <input type="text" value={rowData.id} readOnly />
-        </div>
-        <div>
+         <label>ID:</label>
+         <input type="text" value={rowData.id} readOnly />
+       </div>
+       <div>
           <label>Name:</label>
           <input type="text" value={rowData.name} />
-        </div>
-        <div>
+       </div>
+       <div>
           <label>Sex:</label>
           <input type="text" value={rowData.sex} />
         </div>
         <div>
           <label>Email:</label>
           <input type="text" value={rowData.email} />
-        </div>
-        <div>
-          <label>Salary:</label>
+       </div>
+       <div>
+        <label>Salary:</label>
           <input type="text" value={rowData.salary} />
-        </div>
-        <div>
+       </div>
+       <div>
           <label>Department:</label>
           <input type="text" value={rowData.department} />
         </div>
         <button type="submit">Save</button>
-      </form>
-    </div>
-  );
+     </form>
+   </div>
+</div>
+);
 };
-
 export default EditPage;
