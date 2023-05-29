09c7d06076017f1f2990e96ff6fcbdd8d6b85d99
priyangakrish1608 <103566466+priyangakrish1608@users.noreply.github.com>
2023-05-29 17:09:32 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import { BrowserRouter as Router, useRoutes, Link } from 'react-router-dom';
 import routes from './components/routes';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/employee" className="nav-link">Employee</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/edit" className="nav-link">Edit</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 function App() {
   const routing = useRoutes(routes);
 
-  return routing;
+  return (
+    
+     <div>
+      <Header />
+      {routing}
+    </div>
+    
+  );
 }
 
 export default App;
